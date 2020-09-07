17ff3af51f99f36a6d6fc56b144eec3ab517f29a
Sarah Chun <66026667+sarahschun@users.noreply.github.com>
2020-09-07 12:08:39 -0400

    m

---
+++ build/precache-manifest.54f2a531ec2f34cc9f8519656798a08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b4d95af157e3994b9ee10799f8632c",
+    "url": "/demo_app/index.html"
+  },
+  {
+    "revision": "d44131d81a39d54168de",
+    "url": "/demo_app/static/css/main.0da1e425.chunk.css"
+  },
+  {
+    "revision": "a548bf354eca48c6c6fd",
+    "url": "/demo_app/static/js/2.865d3640.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/demo_app/static/js/2.865d3640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44131d81a39d54168de",
+    "url": "/demo_app/static/js/main.0189ec55.chunk.js"
+  },
+  {
+    "revision": "79eb13d4d500c3fb10de",
+    "url": "/demo_app/static/js/runtime-main.3591049c.js"
+  },
+  {
+    "revision": "899f1779101bbf0fbc97436deace1204",
+    "url": "/demo_app/static/media/image.899f1779.png"
+  }
+]);
